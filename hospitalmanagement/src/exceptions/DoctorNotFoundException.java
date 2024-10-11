7099135d1b623b9f2ce2573773aa3a450c053d96
harshal-jain12 <97157403+harshal-jain12@users.noreply.github.com>
2024-10-11 23:27:54 +0530

    Delete hospitalmanagement directory

--- hospitalmanagement/src/exceptions/DoctorNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package exceptions;
-
-//Custom Exception: DoctorNotFoundException
-public class DoctorNotFoundException extends Exception {
- public DoctorNotFoundException(String message) {
-     super(message);
- }
-}
--- hospitalmanagement/src/exceptions/PatientNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package exceptions;
-
-//Custom Exception: PatientNotFoundException
-public class PatientNotFoundException extends Exception {
- public PatientNotFoundException(String message) {
-     super(message);
- }
-}
-
